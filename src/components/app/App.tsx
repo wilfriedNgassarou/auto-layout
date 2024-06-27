a1724509dd43a43895b2b2a2e3a10def4391c297
WilfriedNgassarou <wilfriedngassarou23@gmail.com>
2024-06-27 17:19:02 +0200

    Ajout de credit

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,6 +3,14 @@ import Layout from "../layout/Layout";
 export default function App() {
   return (
     <section className="app">
+      <div className="credits">
+        <span>
+          Coded by <a href="https://x.com/Wilfried_Ng23" target="_blank" >Wilfried Ngassarou</a>
+        </span>
+        <span>
+          Designed by <a href="https://x.com/EmaniUi/" target="_blank">Emani.Ui</a>
+        </span>
+      </div>
       <Layout />
     </section>
   )
